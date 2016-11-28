963deaf0938344348832dd767f4331adb869ec88
yx2387 <ubuntu@ip-172-31-21-86.ec2.internal>
2016-11-28 00:44:43 +0000

    added socket

--- application.py
+++ application.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template, redirect, abort, url_for
-from flask import request
-from elasticsearch import Elasticsearch, exceptions
-import json
-import requests
+from flask import Flask, render_template, redirect, abort, url_for 
+from flask import request 
+from elasticsearch import Elasticsearch, exceptions 
+import json 
+import requests 
+from flask_socketio import SocketIO, emit
 
-application = Flask(__name__)
+application = Flask(__name__) 
+socketio = SocketIO(application)
 
-es = Elasticsearch()
-keywords = ["love","happy","play", "sleep", "eat", "work", "music", "food", "trump", "clinton"]
+es = Elasticsearch() 
+keywords = ["love","happy","play", "sleep", "eat", "work", "music", 
+"food", "trump", "clinton"] 
+init = True 
 
-@application.route('/')
+@application.route('/') 
 def showGoogleMap():
     query = request.args.get('q', '')
     print query
@@ -21,13 +25,14 @@ def showGoogleMap():
       coords = [{"lat": r["_source"]["location"]["lat"], "lng": r["_source"]["location"]["lon"]} for r in res["hits"]["hits"]]
       results = [r["_source"]["text"] for r in res["hits"]["hits"]]
     coordsJSON = json.dumps(coords).replace('\"','')
-    return render_template("googlemap.html",
+    print coordsJSON
+    print results
+    return render_template("realtime.html",
                            coords=coordsJSON,
                            keywords=keywords,
                            query=query,
-                           results=results)
-
-@application.route('/distance')
+                           results=results) 
+@application.route('/distance') 
 def distance_filter():
     query = request.args.get('q', '')
     search_body = {
@@ -51,48 +56,43 @@ def distance_filter():
                            coords=coordsJSON,
                            keywords=keywords,
                            query=query,
-                           results=results)
+                           results=results) 
 
-
-@application.route('/hello/')
-@application.route('/hello/<name>')
+@application.route('/hello/') 
+@application.route('/hello/<name>') 
 def hello(name=None):
-    return render_template('hello.html', name=name)
+    return render_template('hello.html', name=name) 
 
-
-@application.errorhandler(404)
+@application.errorhandler(404) 
 def page_not_found(error):
-    return 'Oh. Nothing is here.'
+    return 'Oh. Nothing is here.' 
 
-@application.route('/sns', methods = ['GET', 'POST', 'PUT'])
+@application.route('/sns', methods = ['GET', 'POST', 'PUT']) 
 def sns():
     # AWS sends JSON with text/plain mimetype
-    try:
-        js = json.loads(request.data)
-    except:
-        pass
+    global init
+    if init:
+    	try:
+    	    js = json.loads(request.data)
+    	except:
+    	    pass
+	
+    	hdr = request.headers.get('x-amz-sns-message-type')
+    	print (hdr)
+    	# subscribe to the SNS topic
+    	if hdr == 'SubscriptionConfirmation' and 'SubscribeURL' in js:
+    	    r = requests.get(js['SubscribeURL'])
+    	if hdr == 'Notification':
+    	    txt = (js['Message'])
+            lon = (js['MessageAttributes']['Lon']['Value'])
+    	    lat = (js['MessageAttributes']['Lat']['Value'])
+    	    print(js['MessageAttributes']['Id']['Value'])
+    	    senti = (js['MessageAttributes']['Sentiment']['Value'])
+	    socketio.emit('tweet',{'lon':lon,'lat':lat,'text':txt,'senti':senti},namespace='/realtime')
+	
 
-    hdr = request.headers.get('x-amz-sns-message-type')
-    print (hdr)
-    # subscribe to the SNS topic
-    if hdr == 'SubscriptionConfirmation' and 'SubscribeURL' in js:
-        r = requests.get(js['SubscribeURL'])
-
-    if hdr == 'Notification':
-        print(js['Message'])
-	print(js['MessageAttributes']['Lon']['Value'])
-	print(js['MessageAttributes']['Lat']['Value'])
-	print(js['MessageAttributes']['Id']['Value'])
-	print(js['MessageAttributes']['Sentiment']['Value'])
-
-    return 'OK\n'
-
-
-
-
-
+    	return 'OK\n'
 
 
 if __name__ == '__main__':
-    application.run(host='0.0.0.0', port=80, debug=True)
-
+    socketio.run(application,host='0.0.0.0', port=80, debug=True)
---
+++ worker.py
@@ -0,0 +1,91 @@
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import pprint
+import numpy as np
+import elasticsearch
+import boto3
+from watson_developer_cloud import AlchemyLanguageV1
+import multiprocessing
+
+
+session = boto3.Session(
+    aws_access_key_id='AKIAJ5IZKUVUKBGNQC5Q',
+    aws_secret_access_key='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'
+)
+
+es = elasticsearch.Elasticsearch()
+
+if es.indices.exists('t'):
+    es.indices.delete('t')
+mapping = '''
+{
+      "mappings": {
+         "tweet": {
+            "properties": {
+                     "location": {
+                        "type" : "geo_point"
+                  }, "sentiment": {
+                        "type" : "string"
+                  }
+               }
+            }
+         }
+   }
+'''
+es.indices.create(index='t', ignore=400, body=mapping)
+
+alchemy_language = AlchemyLanguageV1(api_key='c9dae3d9a28f23161fa9f8fbc0e0d08c4f6d1fb0')
+sns = boto3.resource('sns', 'us-east-1')
+sqs = boto3.resource('sqs', 'us-east-1')
+queue = sqs.get_queue_by_name(QueueName='Tweet')
+topic = sns.Topic('Tweet')
+
+#pool=multiprocessing.Pool(processes=10)
+#rl =pool.map(run, testFL)
+while(1):
+    for message in queue.receive_messages(MaxNumberOfMessages=1,MessageAttributeNames=['All']):
+    #       print message
+    #        print message.body
+    #        if message.message_attributes is not None:
+    #                print message.message_attributes
+           txt = message.body
+           lat = message.message_attributes.get('Lat').get('StringValue')
+           lon = message.message_attributes.get('Lon').get('StringValue')
+           ID = message.message_attributes.get('Id').get('StringValue')
+           print txt
+           print lat
+           print lon
+           print ID
+           senti = alchemy_language.sentiment(text=message.body,language='english')["docSentiment"]["type"]
+           d = { 'text': txt,"location" : {"lat" : lat,"lon" : lon},"sentiment":senti}
+           #es.index(index='t',doc_type='tweet',id=int(ID),body=d)
+           topic.publish(
+               TopicArn = 'arn:aws:sns:us-east-1:842671666367:Tweet',
+               Message=txt,
+               MessageStructure='string',
+               MessageAttributes={
+                    'Lon': {
+                        'StringValue': lon,
+                        'DataType': 'String'
+                    },
+                    'Lat': {
+                        'StringValue': lat,
+                        'DataType': 'String'
+                    },
+                    'Id': {
+                        'StringValue': ID,
+                        'DataType': 'String'
+                    },
+                    'Sentiment': {
+                        'StringValue': senti,
+                        'DataType': 'String'
+                    }
+            })
+            #message.delete()
+
