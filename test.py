2eb26dad5a1f37ca2441038b5e7b968ee185cf23
JZ <jihaozhang001@gmail.com>
2016-11-29 00:15:48 -0500

    Update test.py

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ import json
 from watson_developer_cloud import AlchemyLanguageV1
 
 session = boto3.Session(
-    aws_access_key_id='AKIAJ5IZKUVUKBGNQC5Q',
-    aws_secret_access_key='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'
+    aws_access_key_id='key',
+    aws_secret_access_key='id'
 )
 alchemy_language = AlchemyLanguageV1(api_key='c9dae3d9a28f23161fa9f8fbc0e0d08c4f6d1fb0')
 sns = boto3.resource('sns', 'us-east-1')
